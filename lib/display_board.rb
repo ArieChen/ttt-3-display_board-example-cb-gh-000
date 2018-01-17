ec0a036f5b94d40dce66220863ea36fc5ec652d9
Arie Tando <13510018@std.stei.itb.ac.id>
2018-01-17 04:35:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-row = "   |   |   "
-line = "-----------"
-
 def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  row = "   |   |   "
+  line = "-----------"
+  puts row
+  puts line
+  puts row
+  puts line
+  puts row
 end
 
 :display_board
